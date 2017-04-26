8a2f4273d6719ec3d2bff6e96a8f2411fef8acf3
Philip Bell <philip.hunter.bell@gmail.com>
2017-04-26 14:42:06 -0400

    revise responsive browsing and nav

--- scrollTo.js
+++ scrollTo.js
@@ -1,10 +1,17 @@
-console.log('test');
-
 let aboutButton = document.querySelector('#aboutButton');
 
-aboutButton.addEventListener('click',() => {
+aboutButton.addEventListener('click', function(e) {
+  console.log(e.target);
   TweenLite.to(window, .75, {
     scrollTo:'.aboutMe',
     ease: Power2.easeOut
   });
 })
+
+// function fix(){
+//   var el = this;
+//   var par = el.parentNode;
+//   var next = el.nextSibling;
+//   par.removeChild(el);
+//   setTimeout(function() {par.insertBefore(el, next);}, 0)
+// }
